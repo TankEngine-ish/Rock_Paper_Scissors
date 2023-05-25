35174af0218c918e4e194d44a335dcc1ff889d17
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2023-05-25 17:38:10 +0300

    Finished

--- index.js
+++ index.js
@@ -48,14 +48,29 @@ function getPlayerChoice (){
     }
 }
 
-
-
 function game () {
+    let scorePlayer = 0;
+    let scoreComputer = 0;
     for (let i = 0; i<5; i++) {
         const playerSelection = getPlayerChoice ();
         const computerSelection = getComputerChoice();
         console.log (playRound (playerSelection, computerSelection));
+
+        if (checkWinner(playerSelection, computerSelection) == "Player"){
+            scorePlayer++
+        }else if (checkWinner(playerSelection, computerSelection) == "Computer"){
+            scoreComputer++;
+        
+        }
     }
+        console.log ("Game Over");
+        if (scorePlayer > scoreComputer){
+            console.log ("Player is the winner!");
+        }else if(scorePlayer<scoreComputer){
+            console.log ("Computer is the winner!");
+        }else{
+            console.log ("It's a Tie!");
+        }
 }
 
 game ()
