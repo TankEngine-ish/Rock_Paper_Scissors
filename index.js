0193aa2f87ba53015053969d28c31b06aee7c791
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2023-05-25 14:35:19 +0300

    Third_Commit_Round_Play

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-const options = ["rock", "paper", "scissors"];
+const options = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice () {
     const choice = options [Math.floor(Math.random () * options.length )];
     return choice
+    console.log ();
     
     }
 
 
-function checkWinner (playerSelection, computerSelection) {
+function checkWinner (playerSelection, computerSelection){
         if (playerSelection == computerSelection){
-        return "Tie";
-    }
-}
+            return "Tie";
+        }else if (
+            (playerSelection == "Rock" && computerSelection == "Scissors") || 
+            (playerSelection == "Scissors" && computerSelection == "Paper") ||
+            (playerSelection == "Paper" && computerSelection == "Rock")
+        ){
+            return "Player";
+        }else {
+            return "Computer";
+        }
+} 
 
 function playRound (playerSelection, computerSelection)  {
-    
-    
+    const result = checkWinner (playerSelection, computerSelection);
+    if (result == "Tie"){
+        return "It's a Tie"
+    }else if (result == "Player"){
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    }else{ 
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    }
 }
+
+getComputerChoice ()
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice ();
+console.log (playRound(playerSelection, computerSelection));
